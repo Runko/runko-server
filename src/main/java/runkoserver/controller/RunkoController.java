4cf935edc2fdf2b508f777ccfecc36c17a95f85b
ArktinenSieni <matti.raty@helsinki.fi>
2015-07-20 11:15:24 +0300

    index-controller

--- src/main/java/runkoserver/controller/RunkoController.java
+++ src/main/java/runkoserver/controller/RunkoController.java
@@ -2,14 +2,16 @@ package runkoserver.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
-public class HelloWorldController {
+@RequestMapping("/")
+public class RunkoController {
     
-    @RequestMapping("/hello")
+    @RequestMapping(method = RequestMethod.GET)
     public String Hello() {
-        return "Hello World!";
+        return "/WEB-INF/templates/index.html";
     }
 }
