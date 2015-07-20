d3b990ba11c32a3e09b2314bb54749605fc98b7c
Warva Henna M <warva@dat-4.cs.helsinki.fi>
2015-07-20 14:38:19 +0300

    Content model and content repository

--- src/main/java/runkoserver/domain/Content.java
+++ src/main/java/runkoserver/domain/Content.java
@@ -5,64 +5,49 @@
  */
 package runkoserver.domain;
 
+import java.util.Date;
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 /**
  *
- * @author Timo
+ * @author warva
  */
-@Entity
-public class Person extends AbstractPersistable<Long> {
 
+@Entity
+public abstract class Content extends AbstractPersistable<Long> {
+    
     @Id
     private Long id;
-    
-    @OneToMany (mappedBy = "owner", fetch = FetchType.EAGER)
-    private List<Content> owned;
 
-    @ManyToMany (mappedBy = "subscribers")
-    private List<Content> subscriptions;
-
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+            
     @NotNull
     @NotEmpty
     private String name;
     
-    @NotNull
-    @NotEmpty
-    private String kuksaId;
-
-  
-
-    public String getKuksaId() {
-        return kuksaId;
-    }
-
-    public void setKuksaId(String kuksaId) {
-        this.kuksaId = kuksaId;
-    }
-
+    @ManyToOne
+    private Person owner;
+    
+    @ManyToMany (mappedBy = "subscriptions")
+    private List<Person> subscribers;
+      
     public String getName() {
         return this.name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-
-    }
+    
 
     public Long getId() {
         return id;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    
 }
