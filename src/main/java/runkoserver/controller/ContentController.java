bbe777b8088b672fbaefcc653f4d2e0623f15f97
genie9 <jenny.lyjina@gmail.com>
2015-07-22 20:38:55 +0300

    changes to views' paths in links to bootstrap and in content controller to simplecontent view

--- src/main/java/runkoserver/controller/ContentController.java
+++ src/main/java/runkoserver/controller/ContentController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/content")
+@RequestMapping("/")
 public class ContentController {
 
     @RequestMapping(value = "/simpleform", method = RequestMethod.GET)
     public String simpleContentForm() {
-        return "simple_content_form";
+        return "/content/simple_content_form";
     }
 }
