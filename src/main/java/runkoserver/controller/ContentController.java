0477a59fbd3f2d5d2f07799d45ef680fd4eb4266
Warva Henna M <warva@dat-4.cs.helsinki.fi>
2015-07-28 15:47:25 +0300

    sisällölle arealistaa

--- src/main/java/runkoserver/controller/ContentController.java
+++ src/main/java/runkoserver/controller/ContentController.java
@@ -10,15 +10,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import runkoserver.domain.SimpleContent;
 import runkoserver.repository.ContentRepository;
+import runkoserver.service.AreaService;
 import runkoserver.service.ContentService;
 
 @Controller
 @RequestMapping("/content")
+
 public class ContentController {
 
     @Autowired
     ContentService contentService;
-
+    
+    @Autowired
+    AreaService areaService;
+    
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String getContent(@PathVariable Long id, Model model) {
         model.addAttribute("content", contentService.findById(id));
@@ -27,7 +32,8 @@ public class ContentController {
     }
     
     @RequestMapping(value = "/simpleform", method = RequestMethod.GET)
-    public String simpleContentForm() {
+    public String simpleContentForm(Model model) {
+        model.addAttribute("area", areaService.findAll());
         return "/content/simple_content_form";
     }
 
