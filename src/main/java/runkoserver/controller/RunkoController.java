2b3a367ff9ed1b555e50b12756e646965607eb24
Warva Henna M <warva@dat-4.cs.helsinki.fi>
2015-07-28 13:52:04 +0300

    RunkoController-muutos

--- src/main/java/runkoserver/controller/RunkoController.java
+++ src/main/java/runkoserver/controller/RunkoController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import runkoserver.service.AreaService;
 import runkoserver.service.ContentService;
 
 @Controller
@@ -14,10 +15,12 @@ public class RunkoController {
     
     @Autowired
     ContentService contentService;
+    AreaService areaService;
     
     @RequestMapping(method = RequestMethod.GET)
     public String Hello(Model model) {
         model.addAttribute("content", contentService.findAll());
+        // model.addAttribute("area", areaService.findAll());
         return "index";
     }
 }
