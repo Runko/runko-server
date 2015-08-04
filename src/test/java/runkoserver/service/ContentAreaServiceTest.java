c1af1163f806ceab31a2192024ed97068b5557a0
Tiaros <timo.a.jaaskelainen@helsinki.fi>
2015-08-04 14:46:15 +0300

    Testing

--- src/test/java/runkoserver/service/ContentAreaServiceTest.java
+++ src/test/java/runkoserver/service/ContentAreaServiceTest.java
@@ -42,31 +42,51 @@ public class ContentAreaServiceTest {
 
     @Test
     public void testFindAllContent() {
-        testSC = contentAreaService.createSimpleContent("Test", "Test", null);
+        testSC = contentAreaService.createSimpleContent("Test", "Find", null);
         contentAreaService.saveContent(testSC);
-        testSC = contentAreaService.createSimpleContent("Test2", "Test", null);
+        testSC = contentAreaService.createSimpleContent("Test 2", "All", null);
         contentAreaService.saveContent(testSC);
         assertEquals(2, contentAreaService.findAllContent().size());
     }
 
     @Test
     public void testFindContentById() {
-        testSC = contentAreaService.createSimpleContent("Testausta", "Test", null);
+        testSC = contentAreaService.createSimpleContent("Test Find", "By Id", null);
         contentAreaService.saveContent(testSC);
         assertEquals(testSC.getName(), contentAreaService.findContentById(testSC.getId()).getName());
     }
 
     @Test
-    public void testDeleteContent() {
-        testSC = contentAreaService.createSimpleContent("Test", "Test", null);
+    public void testDeleteContentWithOu() {
+        testSC = contentAreaService.createSimpleContent("Test", "Delete", null);
         contentAreaService.saveContent(testSC);
         assertTrue(contentAreaService.deleteContent(testSC.getId()));
         assertFalse(contentAreaService.deleteContent(testSC.getId()));
     }
-
-    //Test for areas
     @Test
     public void testCreateSimpleContentWithArea() {
         
     }
+    //Test for areas
+  @Test
+  public void testSaveArea(){
+  testArea= contentAreaService.createArea("Test Save", null, Boolean.TRUE);
+  assertTrue(contentAreaService.saveArea(testArea));
+  }
+  
+  @Test
+  public void testFindAllAreas(){
+  testArea= contentAreaService.createArea("Test Area", null, Boolean.TRUE);
+  contentAreaService.saveArea(testArea);
+  testArea= contentAreaService.createArea("Testi Area 2", null, Boolean.TRUE);
+  contentAreaService.saveArea(testArea);
+  assertEquals(2, contentAreaService.findAllAreas().size());
+  }
+  
+  @Test
+  public void testFindAreaById(){
+  testArea= contentAreaService.createArea("Test Id find", null, Boolean.TRUE);
+  contentAreaService.saveArea(testArea);
+  assertEquals(testArea.getName(), contentAreaService.findAreaById(testArea.getId()).getName());
+  }
 }
