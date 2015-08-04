6c4b8732b29924ac78f64a9bef2f2ed84c51f225
ArktinenSieni <matti.raty@helsinki.fi>
2015-08-04 14:23:19 +0300

    eagerlyerey

--- src/main/java/runkoserver/domain/Area.java
+++ src/main/java/runkoserver/domain/Area.java
@@ -2,6 +2,7 @@ package runkoserver.domain;
 
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -29,7 +30,7 @@ public class Area {
     @ManyToMany
     private List<Person> subscribers;
 
-    @ManyToMany
+    @ManyToMany(fetch=FetchType.EAGER)
     private List<Content> contents;
 
     public Long getId() {
