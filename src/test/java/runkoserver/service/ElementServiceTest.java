67841bb59ab0a06b0c4c0473c1bf60b16d130bad
Henna Warva <henna.warva@iki.fi>
2015-08-27 15:36:04 +0300

    testit menevät läpi

--- src/test/java/runkoserver/service/ElementServiceTest.java
+++ src/test/java/runkoserver/service/ElementServiceTest.java
@@ -42,8 +42,8 @@ public class ElementServiceTest {
 
     @Before
     public void setUp() {
-        areaService.deleteAllAreas();
         elementService.deleteAllElements();
+        areaService.deleteAllAreas();
         testSC = null;
         testArea = null;
         testMan = new Person("Jenny");
@@ -190,5 +190,18 @@ public class ElementServiceTest {
         personService.addBookmark(testMan, content);
                 
         assertEquals(1, testMan.getBookmarks().size());
+    }
+    
+    @Test
+    public void testContentCanBeUnbookmarked() {   
+        doNewAreaAndSave("He-Man2", testMan, Boolean.FALSE);
+        testArea = areaService.findAreaByName("He-Man2");
+        List<Long> areaIds = new ArrayList<>();
+        areaIds.add(testArea.getId());
+        Content content = elementService.createContent("I say HEEYYYYYY", "What's going on?", areaIds, testMan);
+        personService.addBookmark(testMan, content);
+        personService.addBookmark(testMan, content);
+        
+        assertEquals(0, testMan.getBookmarks().size());
     }
 }
